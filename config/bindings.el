0698a78bd67d5619b7f44fa097d9cfcd4e45e55d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-19 09:28:15 +0200

    C-c C-c z create a scala buffer of switch to it if already one.

--- config/bindings.el
+++ config/bindings.el
@@ -2,5 +2,4 @@
 
 ;; For example:
 (define-key scala-mode-map (kbd "C-c C-z") 'ensime-sbt-switch)
-
-;;(define-key scala-mode-map (kbd "C--") 'text-scale-decrease)
+(define-key scala-mode-map (kbd "C-c C-c z") 'ensime-inf-switch);; TODO find a simpler one
