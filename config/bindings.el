66f493791dae9fa60ea041eecfadfc3db5b36e3f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-19 18:25:17 +0200

    Deal with adding asterisk when commenting in multi-line mode.

--- config/bindings.el
+++ config/bindings.el
@@ -3,3 +3,13 @@
 ;; For example:
 (define-key scala-mode-map (kbd "C-c C-z") 'ensime-sbt-switch)
 (define-key scala-mode-map (kbd "C-c C-c z") 'ensime-inf-switch);; TODO find a simpler one
+
+;; Alternatively, bind the 'newline-and-indent' command and
+;; 'scala-indent:insert-asterisk-on-multiline-comment' to RET in
+;; order to get indentation and asterisk-insertion within multi-line
+;; comments.
+(define-key scala-mode-map
+  (kbd "RET") '(lambda ()
+                 (interactive)
+                 (newline-and-indent)
+                 (scala-indent:insert-asterisk-on-multiline-comment)))
