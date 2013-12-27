84553fb8aa577882c03a0442a54dfea2311ef3d3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-27 13:00:58 +0100

    Add the means to clean info log too

--- config/akka/utils.el
+++ config/akka/utils.el
@@ -1,13 +1,24 @@
-(defun akka/clean-debug-log () "Clean some akka debug log."
+(defun akka/clean-info-log () "Clean akka info log."
+  (interactive)
+  (akka/--clean-info-log-remove-debug)
+  (akka/--log-align))
+
+(defun akka/clean-debug-log () "Clean akka debug log."
   (interactive)
   (akka/--clean-debug-log-remove-debug)
-  (akka/--clean-debug-log-align))
+  (akka/--log-align))
 
-(defun akka/--clean-debug-log-remove-debug () "Just clean the logs up."
+(defun akka/--clean-log-with-pattern (pattern) "Just clean all the logs from the current buffer"
   (save-excursion
     (goto-char (point-min))
-    (while (re-search-forward "\\\[DEBUG\\\] .*\n" nil t)
+    (while (re-search-forward pattern nil t)
       (replace-match "" nil nil))))
 
-(defun akka/--clean-debug-log-align () "Just align the remaining logs"
+(defun akka/--clean-debug-log-remove-debug () "Just clean the logs up."
+  (akka/--clean-log-with-pattern "\\\[DEBUG\\\] .*\n"))
+
+(defun akka/--clean-info-log-remove-debug () "Just clean the logs up."
+    (akka/--clean-log-with-pattern "\\\[INFO\\\] .*\n"))
+
+(defun akka/--log-align () "Just align the remaining logs"
   (align-regexp (point-min) (point-max)  "\\(\\s-*\\) - "))
