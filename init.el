3f5aea6ec1ed0b4cd69a4011b9cdc1d02e456be7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 15:55:27 +0100

    Inline code

--- init.el
+++ init.el
@@ -52,9 +52,72 @@
     ;; if you're not using the standard scala mode.
     (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)))
 
-;; Load bindings config
-(live-load-config-file "bindings.el")
-(live-load-config-file "akka/utils.el")
-(live-load-config-file "scala/utils.el")
+(define-key scala-mode-map (kbd "C-c C-z") 'ensime-sbt-switch)
+(define-key scala-mode-map (kbd "C-c C-c z") 'ensime-inf-switch);; TODO find a simpler one
+
+;; Alternatively, bind the 'newline-and-indent' command and
+;; 'scala-indent:insert-asterisk-on-multiline-comment' to RET in
+;; order to get indentation and asterisk-insertion within multi-line
+;; comments.
+(define-key scala-mode-map (kbd "RET") (lambda ()
+                                         (interactive)
+                                         (newline-and-indent)
+                                         (scala-indent:insert-asterisk-on-multiline-comment)))
+
+;; akka utils
+
+(defun akka/clean-info-log () "Clean akka info log."
+  (interactive)
+  (akka/--clean-info-log-remove-debug)
+  (akka/--log-align))
+
+(defun akka/clean-debug-log () "Clean akka debug log."
+  (interactive)
+  (akka/--clean-debug-log-remove-debug)
+  (akka/--log-align))
+
+(defun akka/--clean-log-with-pattern (pattern) "Just clean all the logs from the current buffer"
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward pattern nil t)
+      (replace-match "" nil nil))))
+
+(defun akka/--clean-debug-log-remove-debug () "Just clean the logs up."
+  (akka/--clean-log-with-pattern "\\\[DEBUG\\\] .*\n"))
+
+(defun akka/--clean-info-log-remove-debug () "Just clean the logs up."
+  (akka/--clean-log-with-pattern "\\\[INFO\\\] .*\n"))
+
+(defun akka/--log-align () "Just align the remaining logs"
+  (align-regexp (point-min) (point-max)  "\\(\\s-*\\) - "))
+
+
+;; scala utils
+
+(defun scala/---search-and-replace (pattern-to-search pattern-replace)
+  (save-excursion
+    (while (re-search-forward pattern-to-search nil t)
+      (replace-match pattern-replace nil nil))))
+
+(defun scala/ignore-test-forward () "Ignore the current tests from the current position."
+  (interactive)
+  (scala/---search-and-replace "test(" "ignore("))
+
+(defun scala/active-test-forward () "Unignore the current ignored tests from the current position"
+  (interactive)
+    (scala/---search-and-replace "ignore(" "test("))
+
+(defun scala/switch-log-to-debug () "Switch every log.info( into log.debug("
+  (interactive)
+  (scala/---search-and-replace "log.info(" "log.debug("))
+
+(defun scala/switch-log-to-info () "Switch every log.debug( into log.info("
+  (interactive)
+  (scala/---search-and-replace "log.debug(" "log.info("))
+
+(define-key scala-mode-map (kbd "C-c C-a a")   'scala/active-test-forward)
+(define-key scala-mode-map (kbd "C-c C-a C-a") 'scala/ignore-test-forward)
+(define-key scala-mode-map (kbd "C-c C-a i")   'scala/switch-log-to-info)
+(define-key scala-mode-map (kbd "C-c C-a C-i") 'scala/switch-log-to-debug)
 
 ;;; scala-pack.el ends here
