e4fd07cedd32af85ab6a58d5ca73f269a7d92f35
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-08 19:01:35 +0200

    Refactor - Reindent buffer

--- scala-pack.el
+++ scala-pack.el
@@ -22,19 +22,19 @@
 (setq scala-indent:align-forms t)
 
 (add-hook 'scala-mode-hook '(lambda ()
-  (require 'whitespace)
+                              (require 'whitespace)
 
-  ;; clean-up whitespace at save
-  (make-local-variable 'before-save-hook)
-  (add-hook 'before-save-hook 'whitespace-cleanup)
+                              ;; clean-up whitespace at save
+                              (make-local-variable 'before-save-hook)
+                              (add-hook 'before-save-hook 'whitespace-cleanup)
 
-  (make-local-variable 'whitespace-style)
-  (setq whitespace-style '(face, tabs, trailing))
+                              (make-local-variable 'whitespace-style)
+                              (setq whitespace-style '(face, tabs, trailing))
 
-  ;; turn on highlight. To configure what is highlighted, customize
-  ;; the *whitespace-style* variable. A sane set of things to
-  ;; highlight is: face, tabs, trailing
-  (whitespace-mode)))
+                              ;; turn on highlight. To configure what is highlighted, customize
+                              ;; the *whitespace-style* variable. A sane set of things to
+                              ;; highlight is: face, tabs, trailing
+                              (whitespace-mode)))
 
 ;; ================= ensime
 
@@ -43,7 +43,7 @@
 
 ;; a basic check and warning in case of problem
 (if (not ensime-root)
-  (message "Warning - You must set a variable ENSIME_ROOT pointing to the root of your ensime installation, for example, ~/applications/ensime to benefit from all the setup.")
+    (message "Warning - You must set a variable ENSIME_ROOT pointing to the root of your ensime installation, for example, ~/applications/ensime to benefit from all the setup.")
   (progn
     (message "Proceeding with the scala setup...")
     ;; Load the ensime lisp code...
@@ -70,29 +70,29 @@
 ;; akka utils
 
 (defun akka/clean-info-log () "Clean akka info log."
-  (interactive)
-  (akka/--clean-info-log-remove-debug)
-  (akka/--log-align))
+       (interactive)
+       (akka/--clean-info-log-remove-debug)
+       (akka/--log-align))
 
 (defun akka/clean-debug-log () "Clean akka debug log."
-  (interactive)
-  (akka/--clean-debug-log-remove-debug)
-  (akka/--log-align))
+       (interactive)
+       (akka/--clean-debug-log-remove-debug)
+       (akka/--log-align))
 
 (defun akka/--clean-log-with-pattern (pattern) "Just clean all the logs from the current buffer"
-  (save-excursion
-    (goto-char (point-min))
-    (while (re-search-forward pattern nil t)
-      (replace-match "" nil nil))))
+       (save-excursion
+         (goto-char (point-min))
+         (while (re-search-forward pattern nil t)
+           (replace-match "" nil nil))))
 
 (defun akka/--clean-debug-log-remove-debug () "Just clean the logs up."
-  (akka/--clean-log-with-pattern "\\\[DEBUG\\\] .*\n"))
+       (akka/--clean-log-with-pattern "\\\[DEBUG\\\] .*\n"))
 
 (defun akka/--clean-info-log-remove-debug () "Just clean the logs up."
-  (akka/--clean-log-with-pattern "\\\[INFO\\\] .*\n"))
+       (akka/--clean-log-with-pattern "\\\[INFO\\\] .*\n"))
 
 (defun akka/--log-align () "Just align the remaining logs"
-  (align-regexp (point-min) (point-max)  "\\(\\s-*\\) - "))
+       (align-regexp (point-min) (point-max)  "\\(\\s-*\\) - "))
 
 
 ;; scala utils
@@ -103,20 +103,20 @@
       (replace-match pattern-replace nil nil))))
 
 (defun scala/ignore-test-forward () "Ignore the current tests from the current position."
-  (interactive)
-  (scala/---search-and-replace "test(" "ignore("))
+       (interactive)
+       (scala/---search-and-replace "test(" "ignore("))
 
 (defun scala/active-test-forward () "Unignore the current ignored tests from the current position"
-  (interactive)
-    (scala/---search-and-replace "ignore(" "test("))
+       (interactive)
+       (scala/---search-and-replace "ignore(" "test("))
 
 (defun scala/switch-log-to-debug () "Switch every log.info( into log.debug("
-  (interactive)
-  (scala/---search-and-replace "log.info(" "log.debug("))
+       (interactive)
+       (scala/---search-and-replace "log.info(" "log.debug("))
 
 (defun scala/switch-log-to-info () "Switch every log.debug( into log.info("
-  (interactive)
-  (scala/---search-and-replace "log.debug(" "log.info("))
+       (interactive)
+       (scala/---search-and-replace "log.debug(" "log.info("))
 
 (define-key scala-mode-map (kbd "C-c C-a a")   'scala/active-test-forward)
 (define-key scala-mode-map (kbd "C-c C-a C-a") 'scala/ignore-test-forward)
